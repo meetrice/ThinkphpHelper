c07de6b8a029d90742bfc19d6718ccfa426686b3
meetrice <meetrice@qq.com>
2014-10-07 12:36:49 +0800

    修改了前端引用错误,添加了uniform插件
    Signed-off-by: meetrice <meetrice@qq.com>

--- TPH/Conf/config.php
+++ TPH/Conf/config.php
@@ -1,6 +1,14 @@
 <?php
 return array(
 	//'配置项'=>'配置值'
+	// 添加数据库配置信息
+	'DB_TYPE'=>'mysql',// 数据库类型
+	'DB_HOST'=>'localhost',// 服务器地址
+	'DB_NAME'=>'tpexamples',// 数据库名
+	'DB_USER'=>'root',// 用户名
+	'DB_PWD'=>'root',// 密码
+	'DB_PORT'=>3306,// 端口
+	'DB_PREFIX'=>'think_',// 数据库表前缀
 	'LAYOUT_ON'			=>	true,		//开启布局
 	'LAYOUT_NAME'		=>	'default_layout',	//布局名称layout
 );
